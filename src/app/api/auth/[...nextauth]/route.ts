5fe2addc8bcd419703d7c3a96f6f27e55bca48bd
Bunty <bn.bhandari90@gmail.com>
2025-07-19 18:33:09 +0530

    Fix vercel error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,8 +6,8 @@ const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
           access_type: "offline",
--- src/app/components/LoginButton.tsx
+++ src/app/components/LoginButton.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useSession, signIn, signOut } from "next-auth/react"
+import { Button } from "@/components/ui/button"
 
 export default function LoginButton() {
   const { data: session } = useSession()
